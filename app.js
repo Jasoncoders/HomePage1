0c9fb305481bb91052cc8ff3dc370ccfb7762939
Jasoncoders <leejiho0414@naver.com>
2021-07-27 18:00:07 +0900

    description function created

---
+++ app.js
@@ -0,0 +1,11 @@
+function desc(name) {
+    if(name == 'naver') {
+        window.open('./desc/naverdesc.html', '_self');
+    }
+    if(name == 'google') {
+        window.open('./desc/googledesc.html', '_self');
+    }
+    if(name == 'bing') {
+        window.open('./desc/bingdesc.html', '_self');
+    }
+}
