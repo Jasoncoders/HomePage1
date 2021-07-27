b23f55997b4e4841f9484acad5069918c41c88ae
Jasoncoders <leejiho0414@naver.com>
2021-07-27 22:12:19 +0900

    Good!

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ function desc(name) {
     if(name == 'naver') {
         window.open('./desc/naverdesc.html', '_self');
     }
-    if(name == 'google') {
+    else if(name == 'google') {
         window.open('./desc/googledesc.html', '_self');
     }
-    if(name == 'bing') {
+    else if(name == 'bing') {
         window.open('./desc/bingdesc.html', '_self');
     }
+    else if(name == 'home') {
+        window.open('../index.html', '_self');
+    }
+}
+
+
+function dark() {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+}
+function light() {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
 }
