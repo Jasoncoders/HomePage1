47eab49a89738cc56a8fc8c4f2df006dc8bbdf31
Jasoncoders <leejiho0414@naver.com>
2021-08-03 19:55:20 +0900

    big update! - 2.1.1
    very nice!!

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
 function desc(name) {
-    if(name == 'naver') {
-        window.open('./desc/naverdesc.html', '_self');
-    }
-    else if(name == 'google') {
-        window.open('./desc/googledesc.html', '_self');
-    }
-    else if(name == 'bing') {
-        window.open('./desc/bingdesc.html', '_self');
-    }
-    else if(name == 'home') {
-        window.open('../index.html', '_self');
-    }
-}
-
-
-function dark() {
-    document.body.style.backgroundColor = 'black';
-    document.body.style.color = 'white';
-}
-function light() {
-    document.body.style.backgroundColor = 'white';
-    document.body.style.color = 'black';
-}
-function reset() {
-    document.body.style.backgroundColor = '#614219'
-    document.body.style.color = 'black'
+    window.open(`https://en.wikipedia.org/wiki/${name}`, '_blank')
 }
